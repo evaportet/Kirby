e8ac7fe12b641c13f56eb4a78bfb018c9d602591
Nerea <nerea.martinez@enti.cat>
2024-01-13 15:03:10 +0100

    Bars and init newsScreen

---
+++ app/src/main/java/cdi/kirby/fragments/components/AppNavHost.kt
@@ -0,0 +1,42 @@
+package cdi.kirby.fragments.components
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import cdi.kirby.R
+
+
+class AppNavHost: Fragment() {
+
+    companion object {
+        private lateinit var Instance: AppNavHost
+        fun get() = Instance
+    }
+
+    lateinit var navHost: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Instance = this
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.component_navhost, container, false)
+
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.AppNavigationHostFragment) as NavHostFragment
+        navHost = navHostFragment.navController
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
